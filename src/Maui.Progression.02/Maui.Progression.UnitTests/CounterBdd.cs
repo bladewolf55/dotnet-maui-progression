991e1d562f6d3b9c924b273f78b187e1ac2e2a50
Charles L Flatt <charles@tfhome.us>
2022-06-23 17:28:20 -0600

    Include BDD mspec example

---
+++ src/Maui.Progression.02/Maui.Progression.UnitTests/CounterBdd.cs
@@ -0,0 +1,31 @@
+﻿using Machine.Specifications;
+using Maui.Progression.ViewModels;
+
+namespace Maui.Progression.UnitTests;
+
+[Subject("Scoring")]
+public class When_increasing_counter_by_command
+{
+    static Counter? subject;
+
+    Establish context = () =>
+        subject = new Counter();
+
+    Because of = () =>
+        subject.IncreaseCounterCommand.Execute(null);
+
+    It should_increase_count_by_one = () =>
+        subject.Count.ShouldEqual(1);
+}
+
+
+public class When_initializing_Counter
+{
+    static Counter? subject;
+
+    Establish context = () =>
+        subject = new Counter();
+
+    It should_start_at_zero = () =>
+        subject.Count.ShouldEqual(0);
+}
